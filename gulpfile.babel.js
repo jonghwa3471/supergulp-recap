34ffc358cbcb5b73723d301305d460ae5506f3b5
송종화 <songjonghwa@songjonghwaui-MacBookPro.local>
2024-02-09 20:36:13 +0900

    #4.1

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -5,6 +5,8 @@ import ws from "gulp-webserver";
 import image from "gulp-image";
 import dartSass from "sass";
 import gulpSass from "gulp-sass";
+import autoprefixer from "gulp-autoprefixer";
+import miniCSS from "gulp-csso";
 
 const sass = gulpSass(dartSass);
 
@@ -40,6 +42,8 @@ const styles = () =>
   gulp
     .src(routes.scss.src)
     .pipe(sass().on("error", sass.logError))
+    .pipe(autoprefixer())
+    .pipe(miniCSS())
     .pipe(gulp.dest(routes.scss.dest));
 
 const watch = () => {
